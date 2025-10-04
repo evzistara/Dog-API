83c96138ab87d50c8f205c304234235d96675994
Evzi Stara <evzi.stara@gmail.com>
2025-10-04 12:13:05 +0200

    change fetch to async/await

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const dogFact = document.querySelector('.dog-fact')
-const btn = document.querySelector('button')
+const dogFact = document.querySelector(".dog-fact");
+const btn = document.querySelector("button");
 
-btn.addEventListener('click', fetchDogFact)
+btn.addEventListener("click", fetchDogFact);
 
-async function fetchDogFact(){
-const response = await fetch("https://dogapi.dog/api/v2/facts");
-const data = await response.json();
-let newFact = data.data[0].attributes.body;
-dogFact.textContent = newFact;
-    }
-
+async function fetchDogFact() {
+  const response = await fetch("https://dogapi.dog/api/v2/facts");
+  const data = await response.json();
+  let newFact = data.data[0].attributes.body;
+  dogFact.textContent = newFact;
+}
